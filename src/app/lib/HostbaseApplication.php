8a00d0bfd9cb27984fca88fb7ceaca489467d93c
Ben Getsug <ben@shift31.com>
2014-02-09 17:11:10 -0600

    initial commit

---
+++ src/app/lib/HostbaseApplication.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class HostbaseApplication extends Illuminate\Console\Application {
+
+	protected function getCommandName(\Symfony\Component\Console\Input\InputInterface $input)
+	{
+		return 'hostbase';
+	}
+
+	protected function getDefaultCommands()
+	{
+		// Keep the core default commands to have the HelpCommand
+		// which is used when using the --help option
+		$defaultCommands = parent::getDefaultCommands();
+
+		$defaultCommands[] = new HostbaseCli();
+
+		return $defaultCommands;
+	}
+
+	/**
+	 * Overridden so that the application doesn't expect the command
+	 * name to be the first argument.
+	 */
+	public function getDefinition()
+	{
+		$inputDefinition = parent::getDefinition();
+		// clear out the normal first argument, which is the command name
+		$inputDefinition->setArguments();
+
+		return $inputDefinition;
+	}
+} 
